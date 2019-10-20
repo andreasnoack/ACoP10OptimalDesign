2fe4ddc0947f1ca89472cbe196d7d3e3702abbde
Andreas Noack <andreas@noack.dk>
2019-10-19 22:12:52 -0400

    Add HCV model

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
 t0 = time()
 @info "[Timer: $(round(time() - t0, digits=2))] Loading packages"
-using Pumas, LinearAlgebra
+using Pumas, LinearAlgebra, Plots, Random, Weave
 
 include("warfarin.jl")
+include_weave("hcv.jmd")
--- warfarin.jl
+++ warfarin.jl
@@ -45,56 +45,10 @@ param = (θ₁=0.15,
          Ω=Diagonal([0.07, 0.02, 0.6]),
          σ=0.01)
 
+fim() = Pumas._expected_information_diff(model, data[1], param, zeros(3), Pumas.FO())
 
-function __expected_information(m::PumasModel,
-                                subject::Subject,
-                                param::NamedTuple,
-                                vrandeffsorth::AbstractVector,
-                                ::Pumas.FO,
-                                args...; kwargs...)
-
-  trf = Pumas.toidentitytransform(m.param)
-  vparam = Pumas.TransformVariables.inverse(trf, param)
-
-  # Costruct closure for calling _derived as a function
-  # of a random effects vector. This makes it possible for ForwardDiff's
-  # tagging system to work properly
-  __E_and_V = _param -> Pumas._E_and_V(m, subject, Pumas.TransformVariables.transform(trf, _param), vrandeffsorth, Pumas.FO(), args...; kwargs...)
-
-  # Construct vector of dual numbers for the population parameters to track the partial derivatives
-  cfg = Pumas.ForwardDiff.JacobianConfig(__E_and_V, vparam)
-  Pumas.ForwardDiff.seed!(cfg.duals, vparam, cfg.seeds)
-
-  # Compute the conditional likelihood and the conditional distributions of the dependent variable per observation while tracking partial derivatives of the random effects
-  E_d, V_d = __E_and_V(cfg.duals)
-
-  V⁻¹ = inv(Pumas.cholesky(Pumas.ForwardDiff.value.(V_d)))
-  dEdθ = hcat((collect(Pumas.ForwardDiff.partials(E_k).values) for E_k in E_d)...)
-
-  m = size(dEdθ, 1)
-  n = size(dEdθ, 2)
-  dVpart = similar(dEdθ, m, m)
-  for l in 1:m
-    dVdθl = [Pumas.ForwardDiff.partials(V_d[i,j]).values[l] for i in 1:n, j in 1:n]
-    for k in 1:m
-      dVdθk = [Pumas.ForwardDiff.partials(V_d[i,j]).values[k] for i in 1:n, j in 1:n]
-      # dVpart[l,k] = tr(dVdθk * V⁻¹ * dVdθl * V⁻¹)/2
-      dVpart[l,k] = sum((V⁻¹ * dVdθk) .* (dVdθl * V⁻¹))/2
-    end
-  end
-
-  return dEdθ*V⁻¹*dEdθ', dVpart
-end
-
-function main()
-  tmp1, tmp2 = __expected_information(model, data[1], param, zeros(3), Pumas.FO())
-  tmp1[4:7,4:7] = view(tmp2, 4:7,4:7)
-  # return det(length(data)*tmp2)
-  return rmul!(tmp1, length(data))
-end
-
-@info("[Timer: $(round(time() - t0, digits=2))] Computing block diagonal FIM of warfarin model\nDeterminant of FIM: $(det(main()))")
+@info("[Timer: $(round(time() - t0, digits=2))] Computing block diagonal FIM of warfarin model\nDeterminant of FIM: $(logdet(fim()[1]*length(data)))")
 @info("[Timer: $(round(time() - t0, digits=2))] Timing 1000 evaluations of FIM")
-@time for i in 1:1000; main(); end
+@time for i in 1:1000; fim(); end
 @info("Script completed in $(round(time() - t0, digits=2)) seconds")
 
